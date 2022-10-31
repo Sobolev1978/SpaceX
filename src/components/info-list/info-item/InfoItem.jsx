45e37211965b28065380e9f976deaf0618d235f6
Sobolev1978 <101716379+Sobolev1978@users.noreply.github.com>
2022-10-31 17:43:18 +0300

    fix title

--- src/components/info-list/info-item/InfoItem.jsx
+++ src/components/info-list/info-item/InfoItem.jsx
@@ -2,14 +2,33 @@ import React from 'react';
 import style from './InfoItem.module.scss';
 import cn from 'classnames'
 
-const InfoItem = ({label, value, title, className, subValue }) => {
-  return (
-    <div className={cn(style.content, className)}>
-      <p>{label}</p>
-      <h2>{value} {subValue && (<i>{subValue}</i>)}</h2>
-      <span>{title}</span>
-    </div>
-  );
-};
+const InfoItem = ({
+    label,
+    value,
+    title,
+    className,
+    subValue
+  }) => {
+    return ( <
+        div className = {
+          cn(style.content, className)
+        } >
+        <
+        p > {
+          label
+        } < /p> <
+        h2 > {
+          value
+        } {
+          subValue && ( < i > {
+              subValue
+            } < /i>)}</h2 >
+            <
+            span > {
+              title
+            } < /span> <
+            /div>
+          );
+        };
 
-export default InfoItem;
+        export default InfoItem;
