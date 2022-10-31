f7037caeca6756e9bee866f509cbe042b5d3a7cc
Sobolev1978 <101716379+Sobolev1978@users.noreply.github.com>
2022-10-31 17:49:22 +0300

    fix BrowserRouter  on HashRouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import Header from "./components/header/Header";
 import Container from "./components/container/Container";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
 } from "react-router-dom";
 import MainBlock from "./components/main-block/MainBlock";
 
 
 function App() {
-  return (
-    <Router>
-      <Header/>
-      <Container>
-        <MainBlock />
-      </Container>
-    </Router>
+  return ( <
+    Router >
+    <
+    Header / >
+    <
+    Container >
+    <
+    MainBlock / >
+    <
+    /Container> <
+    /Router>
   );
 }
 
